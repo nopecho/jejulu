81849e02d8d091a03f23b708be030ed6665dc38d
nopecho <ch.jooon@gmail.com>
2022-02-21 18:34:29 +0900

    init project

---
+++ src/main/java/hello/jejulu/JejuluApplication.java
@@ -0,0 +1,13 @@
+package hello.jejulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/jejulu/JejuluApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.jejulu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
