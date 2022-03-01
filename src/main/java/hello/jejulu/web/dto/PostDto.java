3dcf5c89618cdd626b0116dbef0824e47a94ee93
nopecho <ch.jooon@gmail.com>
2022-03-02 00:16:03 +0900

    Feat : Firebase 이미지 저장 서비스

--- src/main/java/hello/jejulu/web/dto/PostDto.java
+++ src/main/java/hello/jejulu/web/dto/PostDto.java
@@ -1,4 +1,17 @@
 package hello.jejulu.web.dto;
 
+import hello.jejulu.domain.post.Category;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
 public class PostDto {
+
+    @Getter @Setter
+    public static class Save{
+        private String title;
+        private Category category;
+        private MultipartFile thumbnail;
+        private String content;
+    }
 }
