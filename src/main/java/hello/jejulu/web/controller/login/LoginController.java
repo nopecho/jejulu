c495d69d3b63967b3e3c4f5f67c84778b0bcc752
nopecho <ch.jooon@gmail.com>
2022-03-03 09:40:00 +0900

    Style : 사용안하는 import 제거

--- src/main/java/hello/jejulu/web/controller/login/LoginController.java
+++ src/main/java/hello/jejulu/web/controller/login/LoginController.java
@@ -7,12 +7,10 @@ import hello.jejulu.web.dto.login.LoginDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
