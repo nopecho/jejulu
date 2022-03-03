b1c8fcfb8e3737b223034d08ce54fb77b90cefb8
nopecho <ch.jooon@gmail.com>
2022-03-03 18:22:45 +0900

    Style : MemberController 주석 추가

--- src/main/java/hello/jejulu/web/controller/member/MemberController.java
+++ src/main/java/hello/jejulu/web/controller/member/MemberController.java
@@ -11,8 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
@@ -23,17 +21,31 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    /**
+     * 회원가입 폼 페이지 요청 핸들러
+     * @param memberSaveDto : 회원 저장 DTO
+     */
     @GetMapping("/sign-up")
     public String memberSaveForm(@ModelAttribute MemberDto.Save memberSaveDto){
         return "jejulu/sign/sign-up-member-form";
     }
 
+    /**
+     * 회원가입시 로그인 아이디 중복 확인 핸들러
+     * @param checkedId :(@ReqeusetParam으로 요청 파라미터값 받아옴) 중복 확인 할 ID
+     */
     @ResponseBody
     @GetMapping("/id-check")
     public boolean memberIdcheck(@RequestParam(name = "v") String checkedId){
         return memberService.isDuplicateId(checkedId);
     }
 
+    /**
+     * 회원 정보 조회 핸들러
+     * @param lookupInfo : @RequestParam 값으로 member요청인지 확인
+     * @param loginMember : @SessionAttribute(name)으로 해당 요청 클라이언트의 세션을 name으로 조회, 로그인 된 회원정보DTO 반환
+     * @param redirectAttributes : redirect시 redirect값을 설정해서 return
+     */
     @GetMapping("/info")
     public String lookupMemberInfo(@RequestParam(name = "v") String lookupInfo,
                                    @SessionAttribute(name = SessionConst.MEMBER) MemberDto.Info loginMember,
@@ -45,6 +57,11 @@ public class MemberController {
         return "redirect:/members/{memberId}";
     }
 
+    /**
+     * 회원 가입 핸들러
+     * @param memberSaveDto : @ModelAttribute로 요청 Form의 input값이랑 해당 객채의 필드랑 바인딩해서 반환
+     * @param bindingResult : 유효성 검증 실패 시 해당 객체에 오류를 담음
+     */
     @PostMapping
     public String memberSave(@ModelAttribute @Validated MemberDto.Save memberSaveDto, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
@@ -55,6 +72,12 @@ public class MemberController {
         return "jejulu/success/success-sign-up";
     }
 
+    /**
+     * 회원 상세 조회 핸들러
+     * @param memberId : @PathVariable = 경로 변수 / 해당 값(회원번호)으로 Repository에서 회원 조회
+     * @param loginMember : @SessionAttribute(name)으로 해당 요청 클라이언트의 세션을 name으로 조회, 로그인 된 회원정보DTO 반환
+     * @param model : 조회 된 회원을 저장하기 위한 Model 객체
+     */
     @GetMapping("/{memberId}")
     public String memberInfo(@PathVariable Long memberId,
                              @SessionAttribute(name = SessionConst.MEMBER) MemberDto.Info loginMember,
@@ -73,6 +96,11 @@ public class MemberController {
         return "jejulu/members/member";
     }
 
+    /**
+     * 회원 수정 폼 요청 핸들러
+     * @param memberId : @PathVariable = 경로 변수 / 해당 값(회원번호)으로 Repository에서 회원 조회
+     * @param model : 조회 된 회원을 저장하기 위한 Model 객체
+     */
     @GetMapping("/{memberId}/edit")
     public String memberUpdateForm(@PathVariable Long memberId, Model model){
         MemberDto lookupMember = memberService.lookupMember(memberId);
@@ -80,6 +108,12 @@ public class MemberController {
         return "jejulu/members/member-update-form";
     }
 
+    /**
+     * 회원 수정 핸들러
+     * @param memberId : @PathVariable = 경로 변수 / 해당 값(회원번호)으로 Repository에서 회원 조회
+     * @param memberUpdateDto : @ModelAttribute로 요청 Form의 input값이랑 해당 객채의 필드랑 바인딩해서 반환
+     * @param session : 회원 수정 완료 시 세션값 업데이트
+     */
     @PatchMapping("/{memberId}")
     public String updateMember(@PathVariable Long memberId,
                                @ModelAttribute MemberDto.Update memberUpdateDto,
