9b16078e03462802e24049407502b4ee8520f21b
nopecho <ch.jooon@gmail.com>
2022-03-31 10:05:15 +0900

    Style : 회원, 게시물 serivce 주석 추가

--- src/main/java/hello/jejulu/service/thumbnail/ThumbnailService.java
+++ src/main/java/hello/jejulu/service/thumbnail/ThumbnailService.java
@@ -17,6 +17,12 @@ public class ThumbnailService {
     private final ThumbnailRepository thumbnailRepository;
     private final FirebaseService firebaseService;
 
+    /**
+     * Thumbnail 생성
+     * @param imageFile Controller 계층의 Dto에서 넘어온 저장할 File객체
+     * @return Thumbnail객체 (Entity)
+     * @throws IOException 파일 입출력 예외
+     */
     public Thumbnail create(MultipartFile imageFile) throws IOException {
         if(imageFile.isEmpty()){
             return null;
@@ -27,6 +33,13 @@ public class ThumbnailService {
         return thumbnailRepository.save(new Thumbnail(thumbnailId, imagePath, originalFilename));
     }
 
+    /**
+     * Thumbnail 수정
+     * @param imageFile Controller 계층의 Dto에서 넘어온 수정할 File객체
+     * @param thumbnail 수정 전 기존 Thumbnail 객체 (Entity)
+     * @return 수정 후 Thumbnail객체
+     * @throws IOException 파일 입출력 예외
+     */
     public Thumbnail update(MultipartFile imageFile, Thumbnail thumbnail) throws IOException {
         if(imageFile.isEmpty()){
             return thumbnail;
@@ -40,12 +53,21 @@ public class ThumbnailService {
         return thumbnail;
     }
 
+    /**
+     * Thumnbnail ID 생성
+     * @param originFileName File객체의 실제 파일명 변수
+     * @return uuid가 적용된 Thumbnail ID
+     */
     private String createThumbnailId(String originFileName){
         String uuid = UUID.randomUUID().toString();
         String extendsName = extracted(originFileName);
         return uuid+"."+extendsName;
     }
 
+    /**
+     * 파일 확장자 추출
+     * @return 확장자 명
+     */
     private String extracted(String originFileName){
         int pos = originFileName.lastIndexOf(".");
         return originFileName.substring(pos+1);
